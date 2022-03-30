32a08a2d57a19accb5920a474f9c91ccbb49a286
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-03-29 20:58:57 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const dotenv = require("dotenv")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL
+  
+).then(()=>console.log("MongoDB connected")).catch((err)=>{
+    console.log("MongoDB Error:",err)
+});
+
+app.use(express.json())
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("backend server running on port: 3001");
+});
---
+++ routes/auth.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+// SIGN UP
+router.post("/signup", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PHRASE_PASS
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOG IN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user &&
+      res.status(401).json("Error entering credentials. Please try again");
+
+    const hashedPass = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PHRASE_PASS
+    );
+    const originalPassword = hashedPass.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Error entering credentials. Please try again");
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        admin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({...others, token});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

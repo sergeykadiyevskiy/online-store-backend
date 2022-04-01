5dff4a9ca0044ca844e8502d481a880fc99575b4
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-03-31 21:20:27 -0400

    updated index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require("dotenv")
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO_URL
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
+app.use("/api/products", productRoute)
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("backend server running on port: 3001");

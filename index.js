87bf7731569233fd5d032e1652927b4c65af9182
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-06-20 12:26:35 -0400

    updated

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const stripeRoute = require("./routes/stripe")
+const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/order")
 const dotenv = require("dotenv")
-const cartRoute = require("./routes/cart")
 const productRoute = require("./routes/product")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
@@ -20,12 +20,12 @@ mongoose.connect(process.env.MONGO_URL
 
 app.use(cors())
 app.use(express.json())
-app.use("/api/checkout", stripeRoute)
 app.use("/api/auth", authRoute)
-app.use("/api/orders", orderRoute)
-app.use("/api/users", userRoute)
 app.use("/api/carts", cartRoute)
+app.use("/api/checkout", stripeRoute)
+app.use("/api/orders", orderRoute)
 app.use("/api/products", productRoute)
+app.use("/api/users", userRoute)
 
 
 app.listen(process.env.PORT || 3001, () => {

ca51bda54e279a9a01fb740bd102b75308b72223
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-06-28 19:51:41 -0400

    updated

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/carts", cartRoute)
-app.use("/api/checkout", stripeRoute)
-app.use("/api/orders", orderRoute)
 app.use("/api/products", productRoute)
+app.use("/api/checkout", stripeRoute)
 app.use("/api/users", userRoute)
+app.use("/api/orders", orderRoute)
 
 
 app.listen(process.env.PORT || 3001, () => {

6149e76d6b9884a91caf4e6c78ae0c34e07c54f1
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-07-06 18:11:02 -0400

    updated

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ mongoose.connect(process.env.MONGO_URL
 app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRoute)
-app.use("/api/products", productRoute)
-app.use("/api/checkout", stripeRoute)
-app.use("/api/users", userRoute)
 app.use("/api/carts", cartRoute)
+app.use("/api/checkout", stripeRoute)
+app.use("/api/products", productRoute)
+app.use("/api/users", userRoute)
 app.use("/api/orders", orderRoute)
 
 

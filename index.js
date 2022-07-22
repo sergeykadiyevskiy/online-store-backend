01e04362e2f6a89f79434f64391beb70cac2afae
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-07-22 17:39:58 -0400

    updated

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
